690f8e82cacc3745bd89273061c941b4b67d2f94
erelin6613 <valentineburn@gmail.com>
2020-11-24 17:26:07 +0200

    Implemented content-based recommendations

--- moviebase.py
+++ moviebase.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 import nltk
 import re
-from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.metrics.pairwise import cosine_similarity
 import pysnooper
 
 dataset_dir = os.path.join(
@@ -16,117 +16,66 @@ title_baselink = 'https://www.imdb.com/title/tt'
 
 class MovieBase:
 
-	def __init__(self, dataset_dir=dataset_dir,
-		dataset_name='small_set_movies',
-		storage_format='csv',
-		rec_df_name='rec_vec_df'):
+	def __init__(self, query_csv='query_df.csv'):
 
-		filename = os.path.join(
-			dataset_dir, dataset_name+'.'+storage_format)
+		self.dataset = pd.read_csv(query_csv)
+		self.__init_matrix()
 
-		if storage_format == 'csv':
-			self.dataset = pd.read_csv(filename)
-		elif storage_format == 'json':
-			self.dataset = pd.read_json(filename)
-		if rec_df_name is not None:
-			self.rec_df = pd.read_csv(os.path.join(dataset_dir, rec_df_name+'.'+storage_format))
+	def __init_matrix(self):
+		self.mapping = pd.Series(
+			self.dataset.reset_index().index, index = self.dataset.title)
+		X = self.dataset.genres.str.split(',', expand=True)[0]
+		X = pd.get_dummies(X)
+		X_reg = pd.get_dummies(self.dataset.region)
+		X = pd.concat([X, X_reg], axis=1)
+		X = pd.concat([X, self.dataset.startYear/2020], axis=1)
+		X = pd.concat([X, self.dataset.averageRating/10], axis=1)
+		X = pd.concat([X, self.dataset.isAdult], axis=1)
+		X = pd.concat(
+			[X, self.dataset.runtimeMinutes/self.dataset.runtimeMinutes.max()],
+			axis=1)
+		self.similarity_matrix = cosine_similarity(X, X)
 
-		self.dataset.loc[:, 'imdbUrl'] = self.dataset['imdbId'].astype(str)
-		self.dataset.loc[:, 'imdbUrl'] = self.dataset['imdbUrl'].apply(pad_to_len)
-		self.dataset.loc[:, 'imdbUrl'] = self.dataset['imdbUrl'].apply(lambda x: title_baselink+x)
-
-		self.rec_df = pd.read_csv(os.path.join(dataset_dir, 'rec_df_small'+'.'+storage_format))
-
-	def __len__(self):
-		return len(self.dataset)
-
-	def _build_title_vocab(self):
-		vocab = []
-
-	def _build_user_matrix(self, movies=None, ratings=None, userId=7777):
-		n_items = self.rec_df['movieId'].unique().shape[0]
-		user_matrix = np.zeros((1, n_items))
-		if movie is None:
-			pass
+	def get_top_rated(self, genre=None, actor=None, num=10):
+		if genre is not None:
+			print(genre)
+			top = self.dataset[self.dataset.genres.str.contains(
+				genre, regex=False, case=False)].sort_values('averageRating')[-num:]
+		elif actor is not None:
+			top = self.dataset[self.dataset.actors.str.contains(
+				actor, regex=False, case=False)].sort_values('averageRating')[-num:]
 		else:
-			for movie, rate in zip(movies, rating):
-				pass
-				#movie, rating = 
-		#vec = pd.DataFrame({'userId': })
-
-
-	def get_movie_genres(self, idx):
-		genres = self.dataset.loc[idx, 'genres'].split('|')
-		return genres
-
-	def get_imdb_link(self, idx):
-		link = self.dataset.loc[idx, 'imdbId']
-		return title_baselink+link
-
-	def get_records(self, idx, columns=['title', 'imdbId']):
-		return self.dataset.loc[idx, columns]
-
-	def list_genres(self):
-		genres = self.dataset.genres.apply(
-			lambda x: x.split('|') if '|' in x else [x])
-		genres_set = set()
-		for g in genres:
-			for each in g:
-				genres_set.add(each)
-		genres_set.remove('(no genres listed)')
-		return list(genres_set)
-
-	def get_top_rated(self, num=10, genre=None, actor=None):
-
-		if genre is None:
-			top = self.dataset.sort_values('median_rate')[-num:]
-		elif genre is not None:
-			genres = self.list_genres()
-			top = pd.DataFrame()
-			while len(top)<num:
-				for genre in genres:
-					top_by_genre = self.dataset[self.dataset['genres'].str.contains(
-						genre.capitalize())].sort_values('median_rate') #[self.dataset.index[-1]]
-					if len(top_by_genre)!=0:
-						top_by_genre = top_by_genre.loc[top_by_genre.index[-1], :]
-						top = top.append(top_by_genre, ignore_index=True)
+			top = self.dataset.sort_values('averageRating')[-num:]
 		if len(top)>num:
 			return top.loc[top.index[:num], :]
 		return top
 
-	@pysnooper.snoop()
 	def get_by_reviewed(self, reviewed, num=10):
 
-		recs = self.rec_df.pivot_table(index ='userId', columns ='title', values ='rating')
 		recommended_df = pd.DataFrame()
+
+		movies_per_rev = round(num/len(reviewed))+1
 
 		while len(recommended_df)<num:
 
 			for movie in reviewed:
-				recommendation = recs.corrwith(recs[movie])
-				recommendation = recommendation.dropna().sort_values(ascending = False).index[:num]
-				recommendation = self.dataset[self.dataset['title'].isin(recommendation)]
-				recommended_df = recommended_df.append(recommendation, ignore_index=True)
-				recommended_df = recommended_df[~recommended_df['title'].isin(reviewed)]
-				recommended_df = recommended_df.drop_duplicates(subset=['title'], keep='first')
-				#print(recommended_df.columns)
+				movie_index = self.mapping[movie]
+				similarity_score = list(enumerate(self.similarity_matrix[movie_index]))
+				similarity_score = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+				similarity_score = similarity_score[1:movies_per_rev]
+				movie_indices = [i[0] for i in similarity_score]
+				print(self.dataset.iloc[movie_indices])
+				recommended_df = recommended_df.append(self.dataset.iloc[movie_indices], ignore_index=True)
 			break
 
-		pred = recommended_df.sort_values(by=['median_rate'], ascending = False)[:num]
-		print(pred[['title', 'median_rate']])
+		pred = recommended_df.sort_values(by=['averageRating'], ascending = False)[:num]
+		#print(pred[['title', 'median_rate']])
 
 		return pred
-
 
 	def lookup_movie(self, query, by='title'):
 
 		movie = self.dataset[self.dataset[by].str.contains(
 			query, regex=False, case=False)]
 
-		return movie['title'].values[0], movie['imdbUrl'].values[0], movie['imdbId'].values[0]
-
-def pad_to_len(value, length=7):
-    value = str(value)
-    while len(value)<length:
-        value = '0'+value
-    return value
+		return movie['title'].values[0], movie['imdbUrl'].values[0]
--- recmoviebot.py
+++ recmoviebot.py
@@ -14,7 +14,6 @@ from telegram.ext import (Updater, CommandHandler,
 	MessageHandler, Filters,ConversationHandler, 
 	CallbackQueryHandler, DictPersistence)
 
-from scraper import *
 from text_correction import fix_sentence
 from utils import get_vocab
 from moviebase import MovieBase
@@ -46,15 +45,20 @@ screen_texts, screen_nums = list(
 	screens.keys()), list(screens.values())
 # print(screens, screen_texts, screen_nums)
 
-buttons = ['How does it work?', 'Add genre', 'Trending movies', 
+buttons = ['How does it work?', 'Add genre', 'Top rated...', 
 		'Show recommendations', 'Add to watch later', 
 		'Add favourite movie', 'To the starting page', 
-		'My watch list', 'Manage my preferences']
+		'My watch list', 'Manage my preferences',
+		'Add actor',
+		'Trending', 'By genre', 'By actor']
 
 starting_buttons = [[buttons[5], buttons[1]],
 					[buttons[2], buttons[3]]]
 
 results_buttons = [[buttons[4], buttons[5]],
+					[buttons[6], buttons[7]]]
+
+add_pref_buttons = [[buttons[1], buttons[-4]],
 					[buttons[6], buttons[7]]]
 
 help_dict = {'/top': 'Show trending movies',
@@ -97,32 +101,18 @@ def error_handler(update, context):
 # @pysnooper.snoop()
 def greet(update, context):
 	"""Greeting handler. Greet a user and display starting layout."""
-	context.bot_data['moviebase'] = MovieBase()
+
+	if 'moviebase' not in context.bot_data.keys():
+		context.bot_data['moviebase'] = MovieBase()
 	markup = ReplyKeyboardMarkup(
-		starting_buttons, one_time_keyboard=True)
+		starting_buttons, 
+		one_time_keyboard=True,
+		resize_keyboard=True)
 	update.message.reply_text(
 		text='I am a RecMovieBot, I can suggest you a new movies to watch.',
 		reply_markup=markup)
 
 	return d['choosing']
-
-#@pysnooper.snoop()
-def show_profile(update, context):
-	"""Greeting handler. Greet a user and display starting layout."""
-
-	buttons = [[
-		InlineKeyboardButton(text=screen_texts[1], callback_data=str(screens['how_work'])),
-		InlineKeyboardButton(text=screen_texts[2], callback_data=str(screens['add_genre']))],
-		[InlineKeyboardButton(text=screen_texts[12], callback_data=str(screens['my_prefs'])),
-		InlineKeyboardButton(text=screen_texts[11], callback_data=str(screens['back']))
-
-	]]
-	keyboard = InlineKeyboardMarkup(buttons)
-	text = 'okay, here is your profile dashboard'
-	update.callback_query.answer()
-	print('show_profile called')
-	update.callback_query.edit_message_text(text=text, reply_markup=keyboard)
-	return d['typing_choice']
 
 # @pysnooper.snoop()
 def display_help(update, context):
@@ -143,7 +133,9 @@ def fallback(update, context):
 	"""Say bye to user."""
 
 	markup = ReplyKeyboardMarkup(
-		starting_buttons, one_time_keyboard=True)
+		starting_buttons, 
+		one_time_keyboard=True, 
+		resize_keyboard=True)
 	context.bot.send_message(chat_id=update.effective_chat.id, 
 		text='See you around')
 
@@ -155,6 +147,21 @@ def explain(update, context):
 	context.bot.send_message(chat_id=update.effective_chat.id, 
 		text=text)
 
+def show_trending(update, context):
+	
+	markup = ReplyKeyboardMarkup(
+		[[buttons[-3], buttons[-2], buttons[-1]]], 
+		one_time_keyboard=True,
+		resize_keyboard=True)
+	message = 'What is that you want?'
+	context.bot.send_message(
+		chat_id=update.effective_chat.id, 
+		text=message,
+		reply_markup=markup)
+	#print(update.message.text)
+	return screens['my_wl']
+
+
 #@pysnooper.snoop()
 def get_top_movies(update, context):
 	"""Display popular movies in the format
@@ -162,17 +169,47 @@ def get_top_movies(update, context):
 	Result is saved as bot_data to acess
 	when needed."""
 
-	markup = ReplyKeyboardMarkup(
-		results_buttons, one_time_keyboard=True)
+	if 'moviebase' not in context.bot_data.keys():
+		context.bot_data['moviebase'] = MovieBase()
 
-	movies = get_listing_items()
-	#m = '\n'.join([k+' - '+v for k, v in movies.items()])
-	movies = context.bot_data['moviebase'].get_top_rated()
+	markup = ReplyKeyboardMarkup(
+		results_buttons, 
+		one_time_keyboard=True,
+		resize_keyboard=True)
+
+	by = update.message.text
+	movies = None
+
+	if by != buttons[-3]:
+		by = by.split(' ')[-1]
+
+		if by=='genre' and 'genre' in context.user_data.keys():
+			movies = context.bot_data['moviebase'].get_top_rated(genre=context.user_data['genre'][0])
+		elif by=='actor' and 'actor' in context.user_data.keys():
+			movies = context.bot_data['moviebase'].get_top_rated(actor=context.user_data['actor'][0])
+		else:
+			markup = ReplyKeyboardMarkup(
+				add_pref_buttons, 
+				one_time_keyboard=True,
+				resize_keyboard=True)
+			text = 'Please add your favourite {} first'.format(by)
+			context.bot.send_message(
+				chat_id=update.effective_chat.id, 
+				text=text,
+				reply_markup=markup)
+			return screens['my_wl']
+
+	if movies is None:			
+		movies = context.bot_data['moviebase'].get_top_rated()
+
 	message = ''
 	for i, mId in enumerate(movies.index):
 		message = message+str(
-			i+1)+'. '+movies.loc[mId, 'title']+'-'+movies.loc[mId, 'imdbUrl']+'\n'
-	context.bot_data['last_recomendation'] = movies 
+			i+1)+'. '+movies.loc[mId, 'title']+' - '+movies.loc[mId, 'imdbUrl']+'\n'
+	context.bot_data['last_recomendation'] = movies
+
+	if message == '':
+		message = 'Ouch, I think I do not know onces you tagged'
 
 	context.bot.send_message(
 		chat_id=update.effective_chat.id, 
@@ -197,13 +234,12 @@ def one_to_watchlist(update, context):
 
 	return d['typing_reply']
 
-def recs_by_genre(update, context):
-	pass
-
 def smart_recommendations(update, context):
 
 	markup = ReplyKeyboardMarkup(
-		results_buttons, one_time_keyboard=True)
+		results_buttons, 
+		one_time_keyboard=True,
+		resize_keyboard=True)
 
 	if 'reviewed' in context.user_data.keys():
 		context.bot.send_message(
@@ -212,14 +248,11 @@ def smart_recommendations(update, context):
 			reply_markup=markup)
 
 		titles = context.user_data['reviewed']
-		#print(titles)
 		movies = context.bot_data['moviebase'].get_by_reviewed(titles)
 	elif 'genre' in context.user_data.keys():
 		movies = context.bot_data['moviebase'].get_top_rated(
 			genre=context.user_data['genre'])
-	#elif 'to_watchlist' in context.user_data.keys():
 
-		#pass
 	else:
 		context.bot.send_message(
 			chat_id=update.effective_chat.id, 
@@ -229,7 +262,7 @@ def smart_recommendations(update, context):
 	message = ''
 	for i, mId in enumerate(movies.index):
 		message = message+str(
-			i+1)+'. '+movies.loc[mId, 'title']+'-'+movies.loc[mId, 'imdbUrl']+'\n'
+			i+1)+'. '+movies.loc[mId, 'title']+' - '+movies.loc[mId, 'imdbUrl']+'\n'
 	context.bot_data['last_recomendation'] = movies 
 
 	context.bot.send_message(
@@ -239,47 +272,6 @@ def smart_recommendations(update, context):
 
 	return screens['my_wl']
 
-# @pysnooper.snoop()
-def basic_recommendations(update, context):
-	"""Display basic recommendation based on the
-	user's favourite genre or actor. Tell the user
-	we know nothing about him/her to recommend
-	anything if there is no data."""
-
-	markup = ReplyKeyboardMarkup(
-		results_buttons, one_time_keyboard=True)
-
-	if 'genre' in list(context.user_data.keys()):
-		genre = context.user_data['genre'][-1]
-		genre = fix_sentence(genre, 'genre')
-		movies = scrape_by_genre(genre)
-		m = '\n'.join(['{}. {} - {}'.format(
-			i+1, k, movies[k]) for i, k in enumerate(movies)])
-		context.bot_data['last_recomendation'] = movies 
-		context.bot.send_message(
-			chat_id=update.effective_chat.id, 
-			text=m,
-			reply_markup=markup)
-
-	elif 'actor' in list(context.user_data.keys()):
-		actor = context.user_data['actor'][-1]
-		movies = scrape_by_cast(actor)
-		m = '\n'.join(['{}. {} - {}'.format(
-			i+1, k, movies[k]) for i, k in enumerate(movies)])
-		context.bot_data['last_recomendation'] = movies 
-		context.bot.send_message(
-			chat_id=update.effective_chat.id, 
-			text=m,
-			reply_markup=markup)
-
-	else:
-		context.bot.send_message(
-			chat_id=update.effective_chat.id, 
-			text='I do not think I have a data to recommend you something')
-
-	return screens['my_wl']
-
-# @pysnooper.snoop()
 def show_users_prefs(update, context):
 	"""Display data bot collected about the user"""
 
@@ -307,13 +299,17 @@ def received_information(update, context):
 	"""Store user's preferences."""
 	user_data = context.user_data
 	text = update.message.text
+	if 'moviebase' not in context.bot_data.keys():
+		context.bot_data['moviebase'] = MovieBase()
 
 	try:
 		print(user_data)
 		category = user_data['choice']
 		if category == 'reviewed':
 			markup = ReplyKeyboardMarkup(
-				starting_buttons, one_time_keyboard=True)
+				starting_buttons, 
+				one_time_keyboard=True,
+				resize_keyboard=True)
 
 			movie = context.bot_data['moviebase'].lookup_movie(text)
 
@@ -321,7 +317,7 @@ def received_information(update, context):
 				message = 'I do not know this one, sorry :('
 				context.user_data['reviewed'].pop(title)
 			else:
-				movie_title, movie_url, movie_id = movie[0], movie[1], movie[2]
+				movie_title, movie_url = movie[0], movie[1]
 				message = "Ah, this one {} Okay, I noted it.".format(movie_url)
 
 			update.message.reply_text(
@@ -339,14 +335,16 @@ def received_information(update, context):
 
 		elif category == 'watch_list':
 			markup = ReplyKeyboardMarkup(
-				results_buttons, one_time_keyboard=True)
+				results_buttons, 
+				one_time_keyboard=True,
+				resize_keyboard=True)
 			movie = context.bot_data['moviebase'].lookup_movie(text)
 
 			if movie is None:
 				message = 'I do not know this one, sorry :('
 				# context.user_data['reviewed'].pop(title)
 			else:
-				movie_title, movie_url, movie_id = movie[0], movie[1], movie[2]
+				movie_title, movie_url = movie[0], movie[1]
 				message = "Great! {} is added to your watchlist.".format(movie_title)
 
 			update.message.reply_text(
@@ -369,14 +367,16 @@ def received_information(update, context):
 			user_data[category] = [text]
 		del user_data['choice']
 		markup = ReplyKeyboardMarkup(
-			starting_buttons, one_time_keyboard=True)
+			starting_buttons, 
+			one_time_keyboard=True,
+			resize_keyboard=True)
 
 		update.message.reply_text(
 			"Neat! I added that to your preferences.",
 			reply_markup=markup)
 	except Exception as e:
 		update.message.reply_text(
-			"Did you choose something? I caught a hiccup")
+			"Please specify the command first")
 		print(e)
 
 	return d['choosing']
@@ -387,9 +387,6 @@ def get_list_choice(update, context):
 	context.user_data['list_choice'] = context.callback_query.data #int(num)-1
 	#return 
 
-def add_item(update, context):
-	pass
-
 # @pysnooper.snoop()
 def add_to_watchlist(update, context):
 	"""Store the bulk of movies to the 
@@ -398,12 +395,16 @@ def add_to_watchlist(update, context):
 	step before."""
 
 	markup = ReplyKeyboardMarkup(
-		starting_buttons, one_time_keyboard=True)
+		starting_buttons, 
+		one_time_keyboard=True,
+		resize_keyboard=True)
 	movies = context.bot_data['last_recomendation']
 	#print(movies)
 	movies_buttons = [[x] for x in movies.keys()]
 	markup_choice = ReplyKeyboardMarkup(
-		movies_buttons, one_time_keyboard=True)
+		movies_buttons, 
+		one_time_keyboard=True,
+		resize_keyboard=True)
 
 	try:
 		assert len(context.user_data['watch_list']) != 0
@@ -421,8 +422,12 @@ def add_to_watchlist(update, context):
 def display_watch_list(update, context):
 	"""Display movies from watch_list in the 
 	format name - link."""
+	if 'moviebase' not in context.bot_data.keys():
+		context.bot_data['moviebase'] = MovieBase()
 	markup = ReplyKeyboardMarkup(
-		starting_buttons, one_time_keyboard=True)
+		starting_buttons, 
+		one_time_keyboard=True,
+		resize_keyboard=True)
 
 	try:
 		assert len(context.user_data['watch_list']) != 0
@@ -463,7 +468,7 @@ def main(production=False):
 	about_handler = CommandHandler('about', explain)
 
 	typing_reply_handler = MessageHandler(
-		(Filters.regex('{}'.format(buttons[1]))),
+		(Filters.regex('{}|{}'.format(buttons[1], buttons[-4]))),
 		regular_choice)
 	review_movie_handler = MessageHandler(
 		(Filters.regex('{}'.format(buttons[5]))),
@@ -476,8 +481,11 @@ def main(production=False):
 			'|'.join([x for x in buttons]))), 
 		received_information)
 
+	choice_movies_handler = MessageHandler(
+		(Filters.regex(buttons[2])), show_trending)
+
 	top_movies_handler = MessageHandler(
-		(Filters.regex(buttons[2])), get_top_movies)
+		(Filters.regex('{}|{}|{}'.format(buttons[-3], buttons[-2], buttons[-1]))), get_top_movies)
 	recommendation_handler = MessageHandler(
 		(Filters.regex(buttons[3])), smart_recommendations)
 
@@ -486,7 +494,7 @@ def main(production=False):
 	display_list_add_handler = MessageHandler(
 		(Filters.regex(buttons[7])), display_watch_list)
 
-	list_choice_handler = CallbackQueryHandler(get_list_choice, pattern='[0-9]')
+	#list_choice_handler = CallbackQueryHandler(get_list_choice, pattern='[0-9]')
 	f_handler = MessageHandler((Filters.regex('bye')), fallback)
 
 	dp.add_handler(prefs_handler)
@@ -494,7 +502,8 @@ def main(production=False):
 	dp.add_handler(about_handler)
 	dp.add_handler(greet_handler)
 	dp.add_handler(review_movie_handler)
-	dp.add_handler(list_choice_handler)
+	dp.add_handler(choice_movies_handler)
+	#dp.add_handler(list_choice_handler)
 	dp.add_handler(typing_reply_handler)
 	dp.add_handler(typing_choice_handler)
 	dp.add_handler(top_movies_handler)
@@ -516,4 +525,4 @@ def main(production=False):
 
 
 if __name__ == '__main__':
-	main(production=False)
+	main(production=True)
